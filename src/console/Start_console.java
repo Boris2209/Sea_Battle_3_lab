df653d977fdd700b3f2a0afb841da8c485af544e
Boris2209 <boris.litvinenko@bk.ru>
2019-12-18 16:11:59 +0300

    Релиз

--- src/console/Start_console.java
+++ src/console/Start_console.java
@@ -1,7 +1,7 @@
 package console;
 
 public class Start_console {
-    public static void main(String[] args){
-        System.out.print("ok");
-    }
+//    public static void main(String[] args){
+//        ConsoleGameModel game = new ConsoleGameModel(10, 10, 4);
+//    }
 }
--- src/swing/FrameGameLauncher.java
+++ src/swing/FrameGameLauncher.java
@@ -4,17 +4,17 @@ import javax.swing.UIManager;
 
 public class FrameGameLauncher {
 
-//    public static void main(String[] args) {
-//        try {
-//            // устанавливаю внешний вид окна приложения
-//            UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsClassicLookAndFeel");
-//        } catch (Throwable e) {
-//            e.printStackTrace();
-//        }
-//        // теперь непосредственно игровое окно
-//        GameModel model = new GameModel(10, 10, 4);
-//        GameView view = new GameView(model);
-//        view.setVisible(true);
-//    }
+    public static void main(String[] args) {
+        try {
+            // устанавливаю внешний вид окна приложения
+            UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsClassicLookAndFeel");
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+        // теперь непосредственно игровое окно
+        GameModel model = new GameModel(10, 10, 4);
+        GameView view = new GameView(model);
+        view.setVisible(true);
+    }
 
 }
